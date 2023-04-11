47642597a37e0486a4019488cf3a140f9943a85e
HolyMolly <104388974+hHolyMolly@users.noreply.github.com>
2023-04-11 21:47:51 +0300

    Add files via upload

--- js/modules/chunks/preloader.js
+++ js/modules/chunks/preloader.js
@@ -1,4 +1,22 @@
+function preloader(speed) {
+   if (document.getElementById("preloader")) {
+      window.addEventListener("load", () => {
+         function loadPage() {
+            return new Promise((resolve) => {
+               setTimeout(resolve, speed);
 
-function preloader(speed){if(document.getElementById("preloader")){window.addEventListener("load",()=>{function loadPage(){return new Promise((resolve)=>{setTimeout(resolve,speed);});}
-loadPage().then(()=>{document.getElementById("preloader").remove();document.body.classList.remove("_lock-scroll");});});}}
+               // Функции которые хотим запустить во время действия прелоадера
+            });
+         }
+
+         loadPage().then(() => {
+            document.getElementById("preloader").remove();
+            document.body.classList.remove("_lock-scroll");
+
+            // Функции которые хотим запустить после прелоадера
+         });
+      });
+   }
+}
+
 export default preloader;
