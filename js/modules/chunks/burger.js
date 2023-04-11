a39e65d1f32bfa5582ebc482f01ece5327b16656
HolyMolly <104388974+hHolyMolly@users.noreply.github.com>
2023-04-11 21:54:32 +0300

    Add files via upload

--- js/modules/chunks/burger.js
+++ js/modules/chunks/burger.js
@@ -1,31 +1,3 @@
-import { bodyLock, bodyUnLock } from "../functions/body_lock.js";
 
-function burger() {
-   const menuBody = document.querySelector("[data-menu]");
-
-   const setOpenMenu = () => {
-      menuBody.classList.add("_active");
-
-      bodyLock();
-   };
-
-   const setCloseMenu = () => {
-      menuBody.classList.remove("_active");
-
-      bodyUnLock();
-   };
-
-   if (menuBody) {
-      document.addEventListener("click", (e) => {
-         const eTarget = e.target;
-
-         if (eTarget.closest("[data-menu-open]")) setOpenMenu();
-
-         if (eTarget.closest("[data-menu-close]")) setCloseMenu();
-
-         if (eTarget.closest("[data-menu-wrapper]")) setCloseMenu();
-      });
-   }
-}
-
+import{bodyLock,bodyUnLock}from"../functions/body_lock.js";function burger(){const menuBody=document.querySelector("[data-menu]");const setOpenMenu=()=>{menuBody.classList.add("_active");bodyLock();};const setCloseMenu=()=>{menuBody.classList.remove("_active");bodyUnLock();};if(menuBody){document.addEventListener("click",(e)=>{const eTarget=e.target;if(eTarget.closest("[data-menu-open]"))setOpenMenu();if(eTarget.closest("[data-menu-close]"))setCloseMenu();if(eTarget.closest("[data-menu-wrapper]"))setCloseMenu();});}}
 export default burger;
--- js/modules/chunks/preloader.js
+++ js/modules/chunks/preloader.js
@@ -1,22 +1,4 @@
-function preloader(speed) {
-   if (document.getElementById("preloader")) {
-      window.addEventListener("load", () => {
-         function loadPage() {
-            return new Promise((resolve) => {
-               setTimeout(resolve, speed);
 
-               // Функции которые хотим запустить во время действия прелоадера
-            });
-         }
-
-         loadPage().then(() => {
-            document.getElementById("preloader").remove();
-            document.body.classList.remove("_lock-scroll");
-
-            // Функции которые хотим запустить после прелоадера
-         });
-      });
-   }
-}
-
+function preloader(speed){if(document.getElementById("preloader")){window.addEventListener("load",()=>{function loadPage(){return new Promise((resolve)=>{setTimeout(resolve,speed);});}
+loadPage().then(()=>{document.getElementById("preloader").remove();document.body.classList.remove("_lock-scroll");});});}}
 export default preloader;
