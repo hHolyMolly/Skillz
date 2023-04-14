f4ebd5028041e095e8c5aaf17f0fd6a7c4289cf3
HolyMolly <developer.code5417@gmail.com>
2023-04-14 17:57:34 +0300

    Updates

--- js/modules/chunks/preloader.js
+++ js/modules/chunks/preloader.js
@@ -1,4 +1,4 @@
 
-import{scrollTo}from"../scripts.js";function preloader(speed){if(document.getElementById("preloader")){window.addEventListener("load",()=>{function loadPage(){return new Promise((resolve)=>{setTimeout(resolve,speed);scrollTo();});}
+import modals from"./modals.js";import{scrollTo,formAction}from"../scripts.js";function preloader(speed){if(document.getElementById("preloader")){window.addEventListener("load",()=>{function loadPage(){return new Promise((resolve)=>{setTimeout(resolve,speed);scrollTo();formAction();modals();});}
 loadPage().then(()=>{document.getElementById("preloader").remove();document.body.classList.remove("_lock-scroll");});});}}
 export default preloader;
